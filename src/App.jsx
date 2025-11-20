c45400301786060ba35297fba4ee0cf4cd03ba9e
AbhidasIron <116381682+AbhidasIron@users.noreply.github.com>
2025-11-20 23:21:05 +0530

    Update App.jsx to update for mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
 import Player from './components/Player';
 import BottomNav from './components/BottomNav';
+import Layout from './components/Layout';
 import Home from './pages/Home';
 
 import { PlaylistProvider } from './context/PlaylistContext';
@@ -16,22 +17,15 @@ function App() {
   return (
     <PlaylistProvider>
       <PlayerProvider>
-        <div className="app-container">
-          <Sidebar />
-
-          <main className="main-content">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/search" element={<Search />} />
-              <Route path="/library" element={<Library />} />
-              <Route path="/liked-songs" element={<LikedSongs />} />
-              <Route path="/playlist/:id" element={<PlaylistDetails />} />
-            </Routes>
-          </main>
-
-          <BottomNav />
-          <Player />
-        </div>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/library" element={<Library />} />
+            <Route path="/liked-songs" element={<LikedSongs />} />
+            <Route path="/playlist/:id" element={<PlaylistDetails />} />
+          </Routes>
+        </Layout>
       </PlayerProvider>
     </PlaylistProvider>
   );
